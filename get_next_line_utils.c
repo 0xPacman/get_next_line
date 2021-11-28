dbf6afdbdec74e7eb84b0b8d2157fe09e56610b1
Ahmed Jadani <ahmed4star@gmail.com>
2021-11-28 13:03:15 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -52,7 +52,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-	if (!s1 || !s2)
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	if (s1 && !s2)
+		return (ft_strdup(s1));
+	if (!s1 && !s2)
 		return (NULL);
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
