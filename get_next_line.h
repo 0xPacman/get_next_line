b57b979d289f3f3452ce2100b7ee87fae466730a
Ahmed Jadani <ahmed4star@gmail.com>
2021-11-29 19:47:11 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,13 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahjadani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/29 17:09:44 by ahjadani          #+#    #+#             */
+/*   Updated: 2021/11/29 17:11:53 by ahjadani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
 
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
+size_t		ft_strlen(const char *s);
+char		*ft_strdup(const char *s1);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char		*ft_strjoin(char *s1, char *s2);
+char		*get_next_line(int fd);
 
 #endif
